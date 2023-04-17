7490aa9ed0cb72a121ce6fd0288ffeec9f2e1285
SANYA KASAUDHAN <kasaudhansanya@gmail.com>
2023-04-17 10:32:03 +0530

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='h-16 w-full bg-blue'>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
