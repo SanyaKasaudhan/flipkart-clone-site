f137efeec5959b9ab3bc8518632b184027b3dfe0
SANYA KASAUDHAN <kasaudhansanya@gmail.com>
2023-04-18 11:52:36 +0530

    item

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-https://www.shutterstock.com/image-vector/vector-88-shopping-day-poster-banner-2012882648
-https://www.shutterstock.com/image-vector/sale-banner-template-design-poster-this-1299986017
-https://www.shutterstock.com/image-vector/gift-coupon-template-50-percent-off-1966689436
-https://www.shutterstock.com/image-vector/smartphone-tablet-laptop-smartwatch-headphones-600w-1641619534.jpg
-https://www.shutterstock.com/image-vector/beauty-make-banner-template-cosmetic-600w-1927734332.jpg
-https://www.shutterstock.com/image-vector/web-site-landing-page-3d-600w-1934575964.jpg
-https://www.shutterstock.com/image-vector/web-page-design-grocery-store-600w-1445961677.jpg
+https://dummyjson.com/products?limit=100
 
 
+https://dummyjson.com/comments?limit=300
+
+
+https://fakestoreapi.com/products/category/jewelery
+
+
+https://fakestoreapi.com/products/category/electronics
+
+https://fakestoreapi.com/products/category/men's_clothing
 
 npm install slick-carousel react-slick
--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const ItemCard = () => {
-  return (
-    <div>
-        <div>
-            <img className='w-20 h-20' alt="photo" src="https://rukminim1.flixcart.com/fk-p-flap/3376/560/image/fef1c23137a2e722.jpg?q=50"/>
-            <div className='font-bold'>Monitor</div>
-            <div className='text-green-700 text-lg'>From Rs. 786</div>
-            <div className="text-gray-500">Lenovo</div>
-        </div>
+    const url='https://dummyjson.com/products?limit=200';
+
+    const [product, setProduct] = useState([]);
+
+    useEffect(() => {
+        fetch(url)
+          .then((response) => response.json())
+          .then((res) => setProduct(res.products))
+          .catch((err) => console.log(err));
+      }, []);
+
+      console.log("pr",product)
+  return (<>
+    <div className='flex flex-row flex-wrap'>
+    {
+        product.map((e) =>( <div className='shadow-lg w-60 h-40 mx-10 my-5 justify-center'>
+            <div className='flex'><img className='w-20 h-20' alt="photo" src={e.thumbnail}/>
+            <span className='flex-row-reverse space-x-reverse'>wis</span></div>
+            <div className='font-bold'>{e.title}</div>
+            <div className='text-green-700 text-lg'>From Rs. {e.price}</div>
+            <div className="text-gray-500">{e.brand}</div>
+        </div>))
+    }
     </div>
+    </>
   )
+  
 }
 
 export default ItemCard
