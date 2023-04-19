2dcd7b4901bb12a5fd9d9a9b155f88635cf4416e
SANYA KASAUDHAN <kasaudhansanya@gmail.com>
2023-04-19 10:10:04 +0530

    Wishlist

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -17,8 +17,8 @@ const ItemCard = () => {
     <div className='flex flex-row flex-wrap'>
     {
         product.map((e) =>( <div className='shadow-lg w-60 h-40 mx-10 my-5 justify-center'>
-            <div className='flex'><img className='w-20 h-20' alt="photo" src={e.thumbnail}/>
-            <span className='flex-row-reverse space-x-reverse'>wis</span></div>
+            <div className='flex space-x-32'><img className='w-20 h-20' alt="photo" src={e.thumbnail}/>
+            <span className='content-end'><img className='w-6 h-6' src="https://cdn-icons-png.flaticon.com/512/25/25424.png" alt="wishlist"/></span></div>
             <div className='font-bold'>{e.title}</div>
             <div className='text-green-700 text-lg'>From Rs. {e.price}</div>
             <div className="text-gray-500">{e.brand}</div>
---
+++ src/components/ItemCardNavigate.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItemCardNavigate = () => {
+  return (
+    <div>ItemCardNavigate</div>
+  )
+}
+
+export default ItemCardNavigate
