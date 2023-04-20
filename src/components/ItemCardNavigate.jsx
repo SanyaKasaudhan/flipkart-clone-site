7db0859824a2368c459cc8b95482831f27d2ac1d
SANYA KASAUDHAN <kasaudhansanya@gmail.com>
2023-04-20 20:49:29 +0530

    card

--- src/components/ItemCardNavigate.jsx
+++ src/components/ItemCardNavigate.jsx
@@ -12,18 +12,25 @@ const ItemCardNavigate = () => {
         return <>Loading.....</>
   return (
     <div className=''>
-        <div className='flex'>
+        
+        <div className='pl-[445px] pt-20 text-4xl font-bold'>{product.brand} {product.category.toUpperCase()}</div>
+        <div className='relative flex ml-40 mt-2'>
+        <div className='flex-col h-20 w-20 mr-5'>{product.images.map((e) => <img className='border-2 border-gray-950 m-2' src={e} alt="imgthumbnail"/> )                
+            }</div>
             <div className=' flex-row'>
+            
             <img src={product.thumbnail} alt="product_img"/>
             </div>
-            <div className=''>
-            <div>{product.title}</div>
-            <div>{product.description}</div>
+            <div className='pl-5 space-y-3'>
+            <div className='text-lg text-amber-600 font-semibold'>{product.title}</div>
+            <div className='text-md text-black font-medium'>{product.description}</div>
             
-            <div>{product.category}</div>
-            <div>{product.discountPercentage}</div>
-            <div>Rs. {product.price}</div>
-            <div>Rating {product.rating}</div>
+            <div className='text-green-700 font-medium text-xl'>Price <span className='text-black'>Rs. {product.discountPercentage}</span>
+             <span className='line-through text-gray-500 pl-4 text-base'>Rs. {product.price}</span></div>
+            <div></div>
+            <div >Rating <span className='w-16 h-14 p-1 text-white bg-green-500 rounded-lg'>{product.rating}</span> </div>
+            <div className='text-lg text-red-600 font-medium'>Hurry only {product.stock} left</div>
+            
             </div>
         </div>
     </div>
